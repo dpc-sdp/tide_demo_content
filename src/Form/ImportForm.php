9b23e44ec2f2151dfd14e2831e59ee62ca2015c5
MdNadimHossain <getovic@gmail.com>
2019-12-02 14:14:18 +1100

    [SDPA-3410] Added Show imported demo content node and media type enti… (#747)
    * [SDPA-3410] Added Show imported demo content node and media type entities id.

--- src/Form/ImportForm.php
+++ src/Form/ImportForm.php
@@ -119,7 +119,17 @@ class ImportForm extends FormBase {
         try {
           $this->contentLoader->setContentPath($temp_dir);
           $entities = $this->contentLoader->loadContent($temp_name);
-          $this->messenger()->addMessage($this->formatPlural(count($entities), '1 demo entity has been imported.', '@count demo entities have been imported.'));
+          $this->messenger()->addMessage($this->formatPlural(count($entities), '1 demo entity has been imported.', '@count demo entities have been imported.') . ' Only node and media type entities are listed below - ');
+          if (!empty($entities)) {
+            foreach ($entities as $entity) {
+              if ($entity->getEntityTypeId() == 'node' || $entity->getEntityTypeId() == 'media') {
+                $this->messenger()->addMessage(t('@entity_type/@entity_id', [
+                  '@entity_type' => $entity->getEntityTypeId(),
+                  '@entity_id' => $entity->id(),
+                ]));
+              }
+            }
+          }
           $form_state->setRebuild(FALSE);
         }
         catch (\Exception $exception) {
