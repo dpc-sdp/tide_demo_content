deb863ce7c376055f8c41cc8fc9d50145f968cfe
vincent-gao <vincent.gao@dpc.vic.gov.au>
2022-03-08 10:57:25 +1100

    Fixes an error caused by a field that is not defined (#23)
    * Fixes an error
    
    * use DJ instead

--- src/EventSubscriber/YamlContent.php
+++ src/EventSubscriber/YamlContent.php
@@ -2,9 +2,12 @@
 
 namespace Drupal\tide_demo_content\EventSubscriber;
 
+use Drupal\Core\Entity\EntityFieldManagerInterface;
+use Drupal\Core\Entity\Exception\UnsupportedEntityTypeDefinitionException;
 use Drupal\Core\Extension\ModuleHandlerInterface;
 use Drupal\tide_demo_content\DemoContentLoader;
 use Drupal\tide_demo_content\DemoContentRepository;
+use Drupal\yaml_content\Event\ContentParsedEvent;
 use Drupal\yaml_content\Event\EntityPostSaveEvent;
 use Drupal\yaml_content\Event\YamlContentEvents;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
@@ -31,16 +34,26 @@ class YamlContent implements EventSubscriberInterface {
   protected $moduleHandler;
 
   /**
+   * The entity field manager.
+   *
+   * @var \Drupal\Core\Entity\EntityFieldManagerInterface
+   */
+  protected $entityFieldManager;
+
+  /**
    * YamlContent constructor.
    *
    * @param \Drupal\tide_demo_content\DemoContentRepository $repository
    *   The demo content repository.
    * @param \Drupal\Core\Extension\ModuleHandlerInterface $module_handler
    *   The module handler service.
+   * @param \Drupal\Core\Extension\EntityFieldManagerInterface $entity_field_manager
+   *   The entity field manager.
    */
-  public function __construct(DemoContentRepository $repository, ModuleHandlerInterface $module_handler) {
+  public function __construct(DemoContentRepository $repository, ModuleHandlerInterface $module_handler, EntityFieldManagerInterface $entity_field_manager) {
     $this->repository = $repository;
     $this->moduleHandler = $module_handler;
+    $this->entityFieldManager = $entity_field_manager;
   }
 
   /**
@@ -49,7 +62,7 @@ class YamlContent implements EventSubscriberInterface {
   public static function getSubscribedEvents() {
     $events = [];
     $events[YamlContentEvents::ENTITY_POST_SAVE][] = ['trackEntity'];
-
+    $events[YamlContentEvents::CONTENT_PARSED][] = ['modifyParsedContent'];
     return $events;
   }
 
@@ -72,6 +85,36 @@ class YamlContent implements EventSubscriberInterface {
         $event->getContentLoader(),
       ]);
     }
+  }
+
+  /**
+   * Remove unsupported fields.
+   */
+  public function modifyParsedContent(ContentParsedEvent $event) {
+    $content = $event->getParsedContent();
+    if (!empty($content)) {
+      foreach ($content as $delta => $details) {
+        if (isset($details['entity']) && isset($details['type'])) {
+          try {
+            $fields_info = $this->entityFieldManager->getFieldDefinitions($details['entity'], $details['type']);
+            $fields = array_keys($fields_info);
+          }
+          catch (\Exception $exception) {
+            throw new UnsupportedEntityTypeDefinitionException();
+          }
+          foreach ($details as $key => $item) {
+            if ($key === 'entity' || $key === 'type') {
+              continue;
+            }
+            if (!in_array($key, $fields)) {
+              unset($content[$delta][$key]);
+            }
+          }
+        }
+      }
+    }
+    $event->setParsedContent($content);
+    return $event;
   }
 
 }
