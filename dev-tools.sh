66acad43951e3daf0cf6c540dbaeea8ced00fbfe
Edward Yuen <edward.yuen@dpc.vic.gov.au>
2022-10-07 12:49:58 +1100

    Temporary dev-tools SHA commit and tide_core D9.4 upgrde.

--- dev-tools.sh
+++ dev-tools.sh
@@ -33,6 +33,6 @@
 # stable version.
 #
 # Uncomment and set the Dev-Tools's commit value and commit this change.
-# export GH_COMMIT=COMMIT_SHA
+export GH_COMMIT=9dbb1773923a6b5ac95583a21cf1b293a593679a
 
 bash <(curl -L https://raw.githubusercontent.com/dpc-sdp/dev-tools/master/install?"$(date +%s)") "$@"
